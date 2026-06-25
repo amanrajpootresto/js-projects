14cb4940b6f9565de77b1f3086f6dec7fa8adc0e
amanrajpootresto <amanrajpoot.resto@gmail.com>
2026-06-25 13:11:50 +0000

    feat: implement CustomError class and demonstrate error handling

--- Test-Project/script.js
+++ Test-Project/script.js
@@ -1,15 +1,72 @@
 console.log("Hii");
 
-try{
-    console.log("Hello, World!", testVariable);
+// try{
+//     const APIError = new Error("This is a test error");
+//     throw APIError;
+// }
+// catch(e){
+//     console.error("An error occurred:", e);
+//     console.log({e});
+//     console.log("Error name:", e.name);
+//     console.log("Error message:", e.message);
+//     console.log("Error stack:", e.stack);
+// }
+
+
+
+
+
+
+
+class CustomError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "CustomError";
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, CustomError);
+        }
+    }
 }
-catch(e){
-    console.error("An error occurred:", e);
-    console.log({e});
-    console.log("Error name:", e.name);
-    console.log("Error message:", e.message);
-    console.log("Error stack:", e.stack);
+
+console.log("\n--- CustomError constructor demo ---");
+try {
+    throw new CustomError("Constructor sets message and name");
+} catch (err) {
+    console.log("Error name:", err.name);
+    console.log("Error message:", err.message);
+    console.log("Error stack:", err.stack);
+    logStackFrames(err);
 }
+
+function vivekName() {
+    console.log("Vivek");
+}
+
+function LogNames(callback = vivekName) {
+    console.log("LogNames function called");
+    callback();
+}
+
+LogNames();
+
+
+
+function fetchUserData(userId, callback) {
+  setTimeout(() => { // Simulating a 2-second network request
+    const data = { id: userId, name: "Alice" };
+    callback(data);
+  }, 2000);
+}
+
+fetchUserData(101, );
+
+
+
+
+
+
+
+
 
 
 
